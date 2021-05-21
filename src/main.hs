d6bd0210e9b7c967ca6ebdaef228833080093f1e
Koumarin <alicefigueiredo837@gmail.com>
2021-05-21 01:58:28 -0300

    initial commit

---
+++ src/main.hs
@@ -0,0 +1,35 @@
+import System.Console.ANSI
+import System.IO
+
+pointAdd :: (Int, Int) -> (Int, Int) -> (Int, Int)
+pointAdd (y, x) (dy, dx) = (y + dy, x + dx)
+
+main :: IO ()
+main = do
+  setTitle "Conway's Game of Life"
+  hSetEcho stdin False
+  hSetBuffering stdin  NoBuffering
+  hSetBuffering stdout NoBuffering
+  showCursor
+  mainloop (0, 0)
+  setSGR [Reset]
+
+mainloop :: (Int, Int) -> IO ()
+mainloop cursor = do
+  c <- hGetChar stdin
+  case c of
+    'h' -> move cursor ( 0, -1)
+    'l' -> move cursor ( 0,  1)
+    'j' -> move cursor ( 1,  0)
+    'k' -> move cursor (-1,  0)
+    'y' -> move cursor (-1, -1)
+    'u' -> move cursor (-1,  1)
+    'b' -> move cursor ( 1, -1)
+    'n' -> move cursor ( 1,  1)
+    'q' -> return ()
+
+move :: (Int, Int) -> (Int, Int) -> IO ()
+move pos delta = let (y, x) = pointAdd pos delta
+                 in do
+  setCursorPosition y x
+  mainloop (y, x)
