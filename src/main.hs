c7b465bd3d7b2b01894254fa80d8853ec62db582
Koumarin <alicefigueiredo837@gmail.com>
2021-05-22 03:15:11 -0300

    fix neighbor counting wasn't using the sliced matrix

--- src/main.hs
+++ src/main.hs
@@ -93,7 +93,7 @@ mooreNeighbors (y, x) state = count neighborhood
     neighborhood = sliceBox (y - 1, x - 1) (y + 1, x + 1) state
     -- Count the number of alive cells.
     count :: [[Bool]] -> Int
-    count blist = foldr (+) 0 (map (\l -> length $ filter (\b -> b) l) state)
+    count blist = foldr (+) 0 (map (\l -> length $ filter (\b -> b) l) blist)
 
 ------------------------------------------------------------
 -- Functions for utilizing arrays.
