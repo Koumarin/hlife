67ad9e31a0636504e36f6b44c3f5bf38898982f4
Koumarin <alicefigueiredo837@gmail.com>
2021-05-21 22:00:35 -0300

    add functions for handling arrays and matrices

--- src/main.hs
+++ src/main.hs
@@ -56,3 +56,23 @@ screenSize = do
 
 pointAdd :: (Int, Int) -> (Int, Int) -> (Int, Int)
 pointAdd (y, x) (dy, dx) = (y + dy, x + dx)
+
+------------------------------------------------------------
+-- Functions for accessing arrays.
+------------------------------------------------------------
+
+at :: Int -> [a] -> a
+at 0 (x:_)  = x
+at n (_:xs) = at (n - 1) xs
+at _ []     = error "Out of bounds!"
+
+atPut :: Int -> a -> [a] -> [a]
+atPut 0 x' (_:xs) = x' : xs
+atPut n x' (x:xs) = x  : (atPut (n - 1) x' xs)
+atPut _ _  []     = error "Out of bounds!"
+
+atyx :: (Int, Int) -> [[a]] -> a
+atyx (y, x) lst = at x (at y lst)
+
+atyxPut :: (Int, Int) -> a -> [[a]] -> [[a]]
+atyxPut (y, x) elt lst = atPut y (atPut x elt (at y lst)) lst
